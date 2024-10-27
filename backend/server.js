7dbc3afeb192a2a64084b10e078a6845319b3834
romanornr <romanornr@gmail.com>
2024-10-27 19:31:27 +0100

    Payment intents endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,42 @@ app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 
 app.post('/create-payment-intent', async (req, res) => {
-    const { amount } = req.body;
+    const {amount} = req.body;
     const paymentIntent = await stripe.paymentIntents.create({
         amount,
         currency: 'eur',
         payment_method_types: ['card_present'],
         capture_method: 'automatic',
     });
-    res.json({ clientSecret: paymentIntent.client_secret });
+    res.json({clientSecret: paymentIntent.client_secret});
 });
+
+app.get('/get-recent-payments', async (req, res) => {
+    try {
+        // Fetch the 10 most recent payments from Stripe
+        const payments = await stripe.paymentIntents.list({
+            limit: 10,
+        });
+
+        res.json({payments: payments.data});
+        } catch (error) {
+            console.error("Error fetching recent payments:", error);
+            res.status(500).json({error: 'Error fetching recent payments'});
+        }
+    });
 
 app.post('/connection_token', async (req, res) => {
     try {
         const connectionToken = await stripe.terminal.connectionTokens.create();
-        res.json({ secret: connectionToken.secret });
+        res.json({secret: connectionToken.secret});
     } catch (error) {
         console.error("Error creating connection token:", error);
-        res.status(500).json({ error: 'Error creating connection token' });
+        res.status(500).json({error: 'Error creating connection token'});
     }
 });
 
 app.get('/get-location-id', (req, res) => {
-    res.json({ locationId: process.env.STRIPE_TERMINAL_LOCATION_ID });
+    res.json({locationId: process.env.STRIPE_TERMINAL_LOCATION_ID});
 })
 
 app.listen(4242, () => console.log('Server running on port 4242'));
